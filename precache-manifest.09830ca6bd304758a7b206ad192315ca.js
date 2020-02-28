44a3133e45a77a216d254caf72b227017cae940b
Gladyshev Roman <romagladyshev@gmail.com>
2020-02-28 15:06:24 +0600

    Updates

---
+++ precache-manifest.09830ca6bd304758a7b206ad192315ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fe865f3911a278475a6e31a2b76913",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "2b0d0b1bca8767b906b7",
+    "url": "/kanban/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "889103f36a7218a2dd13",
+    "url": "/kanban/static/css/main.c70bbfc6.chunk.css"
+  },
+  {
+    "revision": "2b0d0b1bca8767b906b7",
+    "url": "/kanban/static/js/2.7abf7983.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/kanban/static/js/2.7abf7983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889103f36a7218a2dd13",
+    "url": "/kanban/static/js/main.d5bfc2b5.chunk.js"
+  },
+  {
+    "revision": "43faab3350c054999204",
+    "url": "/kanban/static/js/runtime-main.e54c8766.js"
+  }
+]);
